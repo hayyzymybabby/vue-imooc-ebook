379ab4a20171e4f8baf984373764aa54a4246fd8
usamihal <usamihal001@163.com>
2021-08-16 00:23:05 +0800

    准备web字体

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="app">
+    <span class="text">ABCDEFG</span>
     <router-view />
   </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.text {
+  font-family: 'Days One';
+  font-size: 20px;
+  color: orange;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import store from './store'
 // 引入字体图标
 import './assets/styles/icon.css'
+// 引入字体
+import './assets/fonts/daysOne.css'
 
 Vue.config.productionTip = false
 
