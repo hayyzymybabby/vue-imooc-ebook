a718b26cc02c2da543890dc7042c2bca93e51865
usamihal <usamihal001@163.com>
2021-08-16 23:34:21 +0800

    viewport配置和rem配置

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,15 @@
   </div>
 </template>
 
+<script>
+document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html')
+  let fontSize = window.innerWidth / 10
+  fontSize = fontSize > 50 ? 50 : fontSize
+  html.style.fontSize = fontSize + 'px'
+})
+</script>
+
 <style lang="scss" scoped>
 .text {
   font-family: 'Days One';
