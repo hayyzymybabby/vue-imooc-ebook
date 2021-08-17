46db814e48812ee2406c44acd1c49cd6e60f11c2
usamihal <usamihal001@163.com>
2021-08-18 00:33:03 +0800

    引入vuex

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,23 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 document.addEventListener('DOMContentLoaded', () => {
   const html = document.querySelector('html')
   let fontSize = window.innerWidth / 10
   fontSize = fontSize > 50 ? 50 : fontSize
   html.style.fontSize = fontSize + 'px'
 })
+export default {
+  computed: {
+    ...mapGetters(['test'])
+  },
+  mounted () {
+    this.$store.dispatch('setTest', 1).then(() => {
+      console.log(this.test)
+    })
+  }
+}
 </script>
 
 <style lang="scss" scoped>
