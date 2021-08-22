2e30b5246bf338f8610b07c9c4df6430aabeb355
usamihal <usamihal001@163.com>
2021-08-22 13:53:03 +0800

    阅读器解析和渲染

--- src/components/ebook/EbookReader.vue
+++ src/components/ebook/EbookReader.vue
@@ -5,8 +5,31 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import Epub from 'epubjs'
+global.ePub = Epub
 export default {
-
+  computed: {
+    ...mapGetters(['fileName'])
+  },
+  methods: {
+    initEpub () {
+      const url = 'http://192.168.50.236:9001/epub/' + this.fileName + '.epub'
+      this.book = new Epub(url)
+      this.rendition = this.book.renderTo('read', {
+        width: innerWidth,
+        height: innerHeight
+      })
+      console.log(this.rendition)
+      this.rendition.display()
+    }
+  },
+  mounted () {
+    const fileName = this.$route.params.fileName.split('|').join('/')
+    this.$store.dispatch('setFileName', fileName).then(() => {
+      this.initEpub()
+    })
+  }
 }
 </script>
 
