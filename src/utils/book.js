9039c34eaa7417063d61da4b2b4f05dead194351
usamihal <usamihal001@163.com>
2021-09-03 00:18:02 +0800

    目录功能实现-目录数据结构开发

--- src/utils/book.js
+++ src/utils/book.js
@@ -95,3 +95,8 @@ export function getReadTimeByMinute (fileName) {
     return Math.ceil(readTime / 60)
   }
 }
+
+export function flatten (array) {
+  console.log(array)
+  return [].concat(...array.map(item => [].concat(item, ...flatten(item.subitems))))
+}
