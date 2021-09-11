c2b5c9450854c5156a844230b3c75b85fa38b8af
usamihal <usamihal001@163.com>
2021-09-11 16:10:28 +0800

    书签功能实现

--- src/components/ebook/EbookSlideBookmark.vue
+++ src/components/ebook/EbookSlideBookmark.vue
@@ -45,43 +45,43 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .ebook-slide-bookmark {
+.ebook-slide-bookmark {
+  width: 100%;
+  .slide-bookmark-title {
     width: 100%;
-    .slide-bookmark-title {
-      width: 100%;
-      height: px2rem(48);
-      font-size: px2rem(14);
-      font-weight: bold;
-      padding: 0 px2rem(15);
+    height: px2rem(48);
+    font-size: px2rem(14);
+    font-weight: bold;
+    padding: 0 px2rem(15);
+    box-sizing: border-box;
+    @include left;
+  }
+  .slide-bookmark-list {
+    padding: 0 px2rem(15);
+    box-sizing: border-box;
+    .slide-bookmark-item {
+      display: flex;
+      padding: px2rem(15) 0;
       box-sizing: border-box;
-      @include left;
-    }
-    .slide-bookmark-list {
-      padding: 0 px2rem(15);
-      box-sizing: border-box;
-      .slide-bookmrak-item {
-        display: flex;
-        padding: px2rem(15) 0;
-        box-sizing: border-box;
-        .slide-bookmark-item-icon {
-          flex: 0 0 px2rem(29);
-          @include left;
-          .icon-bookmark {
-            width: px2rem(20);
-            height: px2rem(20);
-            font-size: px2rem(12);
-            border-radius: 50%;
-            background: #bbb;
-            @include center;
-          }
+      .slide-bookmark-item-icon {
+        flex: 0 0 px2rem(29);
+        @include left;
+        .icon-bookmark {
+          width: px2rem(20);
+          height: px2rem(20);
+          font-size: px2rem(12);
+          border-radius: 50%;
+          background: #bbb;
+          @include center;
         }
-        .slide-bookmark-item-text {
-          font-size: px2rem(14);
-          line-height: px2rem(15);
-          max-height: px2rem(45);
-          @include ellipsis2(3);
-        }
+      }
+      .slide-bookmark-item-text {
+        font-size: px2rem(14);
+        line-height: px2rem(15);
+        max-height: px2rem(45);
+        @include ellipsis2(3);
       }
     }
   }
+}
 </style>
